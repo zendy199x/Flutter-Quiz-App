169613f122abe86b1f55956bc313ef58440e0008
Zendy <nguyenduc.051194@gmail.com>
2021-08-13 17:59:32 +0700

    feat: Project setup

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color kSecondaryColor = Color(0xFF8B94BC);
+const Color kGreenColor = Color(0xFF6AC259);
+const Color kRedColor = Color(0xFFE92E30);
+const Color kGrayColor = Color(0xFFC1C1C1);
+const Color kBlackColor = Color(0xFF101010);
+const LinearGradient kPrimaryGradient = LinearGradient(
+  colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const double kDefaultPadding = 20.0;
---
+++ lib/models/questions.dart
@@ -0,0 +1,40 @@
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({
+    required this.id,
+    required this.question,
+    required this.answer,
+    required this.options,
+  });
+}
+
+const List sampleData = [
+  {
+    "id": 1,
+    "question":
+        "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "What command do you use to output data to the screen?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answer_index": 2,
+  },
+];
