29d8de36246d3398d851282875c6934fa44f90ff
Zendy <nguyenduc.051194@gmail.com>
2021-08-14 13:58:11 +0700

    feat: Welcome screen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:quiz_app/constants.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            "assets/icons/bg.svg",
+            width: double.infinity,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(flex: 2),
+                  Text(
+                    "Let's Play Quiz,",
+                    style: Theme.of(context).textTheme.headline4!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const Text("Enter your informations below"),
+                  const Spacer(),
+                  TextField(
+                    onChanged: (value) {
+                      // Name value
+                    },
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(kDefaultPadding * 0.75),
+                      decoration: const BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Let's Start Quiz",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button!
+                            .copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
